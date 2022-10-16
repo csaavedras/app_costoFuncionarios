f9804b9d2e165786230a19c28253603ffe57bce2
Camilo Saavedra <ci.saavedras@gmail.com>
2022-10-16 12:25:45 -0400

    first commit

---
+++ WindowsFormsApp_horasExtras/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp_horasExtras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(151, 181);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(167, 42);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(355, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.GridColor = System.Drawing.Color.White;
+            this.dataGridView.Location = new System.Drawing.Point(151, 259);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(935, 318);
+            this.dataGridView.TabIndex = 2;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 652);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ WindowsFormsApp_horasExtras/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_horasExtras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            string sqlCredencial = "SERVER=LENOVO_S940;DataBase=horas_extras; Integrated Security=SSPI";
+            SqlConnection Conexion = new SqlConnection(sqlCredencial);
+            Conexion.Open();
+
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("Select nombre_mes from dias order by mes", Conexion);
+            myReader = myCommand.ExecuteReader();
+
+
+            while (myReader.Read())
+            {
+                comboBox1.Items.Add(myReader["nombre_mes"].ToString());
+            }
+            Conexion.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Selecciona el mes";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FORMA DE AGREGAR COLUMNAS AL DATA GRID
+            dataGridView.Columns.Add("rut", "Rut");
+            dataGridView.Columns.Add("nombre", "NOMBRE");
+            dataGridView.Columns.Add("horas_extras", "HORAS EXTRAS");
+            dataGridView.Columns.Add("costo", "COSTO");
+
+            //FORMA DE AGREGAR DATOS A LAS FILAS DEL DATA GRID
+            dataGridView.Rows.Add("18.954.102-3", "Camilo", "100", "$24.000");
+
+
+
+            string sqlCredencial = "SERVER=LENOVO_S940;DataBase=horas_extras; Integrated Security=SSPI";
+            SqlConnection Conexion = new SqlConnection(sqlCredencial);
+            Conexion.Open();
+
+            SqlDataReader myReader = null;
+            //SQL COMANDS
+            SqlCommand myCommand = new SqlCommand("Select fecha from registro", Conexion);
+
+            SqlCommand myCommandGetRut = new SqlCommand("select rut, nombre from funcionarios", Conexion);
+
+            myReader = myCommandGetRut.ExecuteReader();
+            //myReader = myCommand.ExecuteReader();
+
+
+            while (myReader.Read())
+            {
+
+                string rut = myReader["rut"].ToString();
+                string nombre = myReader["nombre"].ToString();
+                dataGridView.Rows.Add(rut, nombre);
+
+
+
+            }
+            Conexion.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
